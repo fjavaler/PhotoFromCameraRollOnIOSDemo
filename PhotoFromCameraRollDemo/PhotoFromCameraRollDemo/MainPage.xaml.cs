3f0b5a2883efe9955a3779eac82bac85b68787b2
Frederick Javalera <frederickjavalera@gmail.com>
2020-02-05 15:16:13 -0700

    Add project files.

---
+++ PhotoFromCameraRollDemo/PhotoFromCameraRollDemo/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PhotoFromCameraRollDemo
+{
+  // Learn more about making custom code visible in the Xamarin.Forms previewer
+  // by visiting https://aka.ms/xamarinforms-previewer
+  [DesignTimeVisible(false)]
+  public partial class MainPage : ContentPage
+  {
+    public MainPage()
+    {
+      InitializeComponent();
+    }
+
+    async void OnPickPhotoButtonClicked(object sender, EventArgs e)
+    {
+      (sender as Button).IsEnabled = false;
+
+      Stream stream = await DependencyService.Get<IPhotoPickerService>().GetImageStreamAsync();
+      if (stream != null)
+      {
+        image.Source = ImageSource.FromStream(() => stream);
+      }
+
+    (sender as Button).IsEnabled = true;
+    }
+  }
+}
